927da58479a02c471555e98c89a4dda05a50210f
Adrian Brightmoore <abrightmoore@yahoo.com.au>
2020-08-13 09:24:51 +1000

    Add files via upload

---
+++ WorldDataDemo/WorldDataDemo.py
@@ -0,0 +1,14 @@
+# @TheWorldFoundry
+
+def perform(level,box,options):
+	print level.GameType
+	print level.GeneratorName
+	print level.Height
+	print level.LastPlayed
+	print level.Length
+	print level.LevelName
+	print level.RandomSeed
+	print level.SizeOnDisk
+	print level.Time
+	print level.Width
+	print level.size
